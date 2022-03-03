af9e745d75f04233dbe3862c640d74f1a6f71d66
pia9322 <77098879+pia9322@users.noreply.github.com>
2022-03-03 17:03:25 +0900

    load
    load

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const WebSocket = require('ws');
+const socket = new WebSocket.Server({port: 8081});
+
+
+app.use("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080);
+
+socket.on('connection', (ws, req) => {
+  
+
+  // 에러 발생 시
+  ws.on('error', (err) => {
+    console.error(err);
+  });
+
+  // 연결 종료 시
+  ws.on('close', () => { 
+    console.log('클라이언트 접속 해제'/* , ip */);
+    clearInterval(ws.interval);
+  });
+
+
+  ws.on('message', (message) => { // 클라이언트로부터 메시지 수신 시
+    console.log(message);
+  });
+  
+  // send
+  ws.interval = setInterval(() => {
+    if (ws.readyState === ws.OPEN) {
+      ws.send('서버:');
+      // 공백
+    }
+  }, 3000);
+
+});
+
